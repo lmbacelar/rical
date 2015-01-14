884a9727bce032e0451c6801421b1a7f62b8327c
Luis Bacelar <lmbacelar@gmail.com>
2015-01-14 22:07:58 +0000

    array, hash arguments fargs, dfargs passed to f, df

--- lib/rical.rb
+++ lib/rical.rb
@@ -4,16 +4,18 @@ require 'rical/errors'
 module Rical
   extend self
 
-  def root_for f:, df: nil, x0: 0.0, x1: 1.0, method:, num: 100, err: 1e-6
+  def root_for f:, fargs: nil,
+               df: nil, dfargs: nil,
+               x0: 0.0, x1: 1.0, method:, num: 100, err: 1e-6
     raise ArgumentError, 'f must respond_to call'    unless f.respond_to? :call
 
     case method
     when :n, :newton, :newton_raphson
       raise ArgumentError, 'df is required'          unless df
       raise ArgumentError, 'df must respond_to call' unless df.respond_to? :call
-      newton_raphson_root_for f, df, Float(x0), num, err
+      newton_raphson_root_for f, fargs, df, dfargs, Float(x0), num, err
     when :s, :sec, :secant
-      secant_root_for f, Float(x0), Float(x1), num, err
+      secant_root_for f, fargs, Float(x0), Float(x1), num, err
     else
       raise ArgumentError, 'unexpected method (allowed :newton, :secant)'
     end
@@ -26,24 +28,33 @@ module Rical
   alias_method :inv_for, :inverse_for
 
 private
-  def newton_raphson_root_for f, df, x, n, err
+  def newton_raphson_root_for f, fargs, df, dfargs, x, n, err
     n.times do
-      dfx = df.call x
+      fx  = compute  f, x, fargs
+      dfx = compute df, x, dfargs
       dfx = [1e-3, err * 1e3].max if dfx == 0.0
-      x = x - ( f.call(x) / dfx )
-      return x if f.call(x).abs < err
+      x = x - ( fx / dfx )
+      return x if compute(f, x, fargs).abs < err
     end
     raise NoConvergenceError
   end
 
-  def secant_root_for f, x0, x1, n, err
+  def secant_root_for f, fargs, x0, x1, n, err
     n.times do
-      delta = f.call(x1) - f.call(x0)
+      fx0 = compute f, x0, fargs
+      fx1 = compute f, x1, fargs
+      delta = fx1 - fx0
       delta = [1e-3, err * 1e3].max if delta == 0.0
-      x = x1 - (f.call(x1) * (x1 - x0)) / delta
-      return x if f.call(x).abs < err
+      x = x1 - (fx1 * (x1 - x0)) / delta
+      return x if compute(f, x, fargs).abs < err
       x0, x1 = x1, x
     end
     raise NoConvergenceError
+  end
+
+  def compute f, x, fargs
+    return f.call x          unless fargs
+    return f.call x, **fargs if     fargs.is_a? Hash
+    return f.call x, *fargs
   end
 end
